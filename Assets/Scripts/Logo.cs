bafa1bec27e7534c98d153c3913b3b1f47141468
MaxPlayer2011 <maximus.naumus@gmail.com>
2021-10-22 17:51:29 -0400

    added skip logo feature

--- Assets/Scripts/Logo.cs
+++ Assets/Scripts/Logo.cs
@@ -9,6 +9,19 @@ public class Logo : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         yield return new WaitForSeconds(5f);
+        LoadGame();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Skip Logo"))
+        {
+            LoadGame();
+        }
+    }
+
+    void LoadGame()
+    {
         if (PlayerPrefs.GetInt("hasPlayedBefore") == 0 & PlayerPrefs.GetInt("debug") == 1)
         {
             SceneManager.LoadScene("FirstTime");
