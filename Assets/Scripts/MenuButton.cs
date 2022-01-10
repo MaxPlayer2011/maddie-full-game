fbf23fbf96fd0f6a75e326fc895ebe66dc3b4124
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-01-10 10:09:10 -0500

    improve mainmenu navigation

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
 public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Button button;
+    private bool mouseOverButton;
     private TextMeshProUGUI text;
-    private EventTrigger trigger;
     
     private void Start()
     {
-        button = GetComponent<Button>();
         text = GetComponentInChildren<TextMeshProUGUI>();
-        trigger = gameObject.AddComponent<EventTrigger>();
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == gameObject)
+        {
+            text.fontStyle = FontStyles.Underline;
+        }
+
+        else
+        {
+            if (mouseOverButton == false)
+            {
+                text.fontStyle = FontStyles.Normal;
+            }
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        mouseOverButton = true;
         text.fontStyle = FontStyles.Underline;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        mouseOverButton = false;
         text.fontStyle = FontStyles.Normal;
     }
 }
